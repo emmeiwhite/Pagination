6cc59bfaa8d021666f44371a28a1a1efbeedfa71
Imran Rafiq Rather <emmeiwhite@gmail.com>
2021-01-28 06:01:23 +0530

    API integrated and mapped with component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
+
 function App() {
-  return <h2>pagination starter</h2>
+  const [people, setPeople] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { loading, data } = useFetch(url);
+
+  useEffect(() => {
+    if (data) {
+      setIsLoading(false);
+      setPeople(data);
+    }
+  }, []);
+
+  console.log("MAIN DATA ...");
+  console.log(data);
+  return (
+    <main className="section">
+      <h3 className="section-title">Pagination | Gift for Brother</h3>
+
+      <div className="underline"></div>
+      <section className="followers container">
+        {data &&
+          data.map((item) => {
+            return <Follower {...item} key={item.id} />;
+          })}
+      </section>
+
+      {/* Pagination Code */}
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/Follower.js
+++ src/Follower.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Follower = () => {
-  return <h2>follower component</h2>
-}
+const defaultImage = "https://picsum.photos/200";
+const Follower = ({ avatar_url, login }) => {
+  return (
+    <article className="card">
+      <img src={avatar_url || defaultImage} alt="Github User" />
+      <p>{login || "Default Name"}</p>
+      <button className="btn">View Profile</button>
+    </article>
+  );
+};
 
-export default Follower
+export default Follower;
